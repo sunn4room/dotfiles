139ca280d233bc3b984a79750185599e130fbb5f
sunn4room <sunn4room@163.com>
2024-07-09 18:13:47 +0800

    remove qutebrowser

--- dot_config/private_qutebrowser/exact_greasemonkey/ac-hsck.js
+++
@@ -1,6 +0,0 @@
-// ==UserScript==
-// @name          ac hsck
-// @include       http://hsck*.cc/*
-// ==/UserScript==
-
-document.getElementsByTagName("body")[0].children[1].scrollIntoView()
--- dot_config/private_qutebrowser/exact_greasemonkey/vip-darker.js
+++
@@ -1,6 +0,0 @@
-// ==UserScript==
-// @name          vip darker
-// @include       https://vip.yeyulingfeng.com/
-// ==/UserScript==
-
-document.getElementsByTagName("body")[0].setAttribute("style", "background: #000000");
